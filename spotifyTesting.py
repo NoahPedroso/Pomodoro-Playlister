bf36e75175e48527f8a9fced91251d2c78f3c48e
Noah Pedroso <n.pedroso@me.com>
2024-05-20 15:34:36 -0400

    Added licensing heading & description

--- spotifyTesting.py
+++ spotifyTesting.py
@@ -1,3 +1,9 @@
+'This is the main script to interact with Spotipy API. It is currently just testing.'
+'''Copyright (c) 2024, Noah Pedroso
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. '''
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
